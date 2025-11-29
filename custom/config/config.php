d5dcc1922fab049e9988feb332b19b8e395fc98d
Erica Deossa <erica1830@hotmail.com>
2025-11-29 12:48:35 -0500

    Add config for Render deployment

---
+++ custom/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'database' => [
+        'driver' => 'pdo_pgsql',
+        'host' => 'dpg-d4l3dcbe5dus73fi6vd0-a',
+        'port' => 5432,
+        'dbname' => 'espocrm_yf42',
+        'user' => 'espouser',
+        'password' => 'J3q7c9HABfyDwv6GnGKC1zYeKZEuyPyH',
+    ],
+    'useCache' => false,
+];
